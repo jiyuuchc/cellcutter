dfa280ea612560095ee2fc6614f1d635b5da676f
Ji Yu <jyu@uchc.edu>
2021-03-18 22:21:27 -0400

    Add a simple supervised training function

--- cellcutter/train.py
+++ cellcutter/train.py
@@ -7,3 +7,17 @@ def augment(img, t):
   if t == 2 or t == 3:
     img = tf.image.flip_up_down(img)
   return img
+
+def train_with_label(data, model, epochs = 1, batch_size = 256):
+  model.compile(
+      optimizer='Adam',
+      loss=tf.losses.BinaryCrossentropy(from_logits=True),
+      metrics=['accuracy'],
+      )
+  dataset = data.tf_dataset_with_label()
+
+  for epoch in range(epochs):
+    print('Epoch : #%i'%epoch)
+    model.fit(dataset.batch(batch_size))
+
+  model.summary()
