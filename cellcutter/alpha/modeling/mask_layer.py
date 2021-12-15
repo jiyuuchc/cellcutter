ef9f13edcc7c3162708deabd1cd4ad223320da4b
Ji Yu <jyu@uchc.edu>
2021-12-15 14:07:51 -0500

    correct implementation of mask layer

--- cellcutter/alpha/modeling/mask_layer.py
+++ cellcutter/alpha/modeling/mask_layer.py
@@ -1,13 +1,71 @@
 import tensorflow as tf
+from .common import *
+from ..ops import *
+import skimage.transform
+
+def decode_one_img(masks_one_img, bboxes_one_img, scores_one_img, threshold, h, w):
+    bboxes_one_img = tf.ensure_shape(bboxes_one_img, (None,4))
+    def decode_one_mask(inputs):
+        mask, box = inputs
+        if box[2] == box[0] or box[3] == box[1]:
+            return tf.zeros((h,w), tf.bool)
+        if box[2] > box[0]:
+            r0 = box[0]
+            r1 = box[2]
+        else:
+            r0 = box[2]
+            r1 = box[0]
+            mask = mask[::-1,:]
+        if box[3] > box[1]:
+            c0 = box[1]
+            c1 = box[3]
+        else:
+            c0 = box[3]
+            c1 = box[1]
+            mask = mask[:,::-1]
+        mask = tf.image.resize(mask, [r1-r0, c1-c0])
+        mi = tf.where(mask[:,:,0] >= .5) + [r0,c0]
+        indices = tf.where((mi[:,0]>=0) & (mi[:,0]<h) & (mi[:,1]>=0) & (mi[:,1]<w))
+        if tf.size(indices) == 0:
+            return tf.zeros((h,w), tf.bool)
+        mi = tf.gather_nd(mi, indices)
+        return tf.scatter_nd(mi, tf.ones((tf.shape(mi)[0],), tf.bool), [h,w])
+
+    bboxes_one_img = tf.cast(tf.math.round(bboxes_one_img * [h, w, h, w]), tf.int32)
+    mask_stack = tf.map_fn(
+        decode_one_mask,
+        [tf.cast(masks_one_img, tf.float32), bboxes_one_img],
+        fn_output_signature=tf.TensorSpec((h, w), tf.bool),
+    )
+    mask_areas = tf.math.count_nonzero(mask_stack, axis=(1,2))
+
+    def iou_one_row(inputs):
+        one_mask, one_mask_area = inputs
+        intersects = tf.math.count_nonzero(one_mask & mask_stack, axis=(1,2))
+        union = one_mask_area + mask_areas - intersects
+        return tf.cast(intersects, tf.float32) / (tf.cast(union, tf.float32) + 1.0e-7)
+    ious = tf.map_fn(
+        iou_one_row,
+        [mask_stack, mask_areas],
+        fn_output_signature=tf.TensorSpec((None,), tf.float32),
+    )
+
+    indices = tf.image.non_max_suppression_overlaps(ious, scores_one_img, 2000, overlap_threshold=threshold)
+    mask_stack = tf.gather(mask_stack, indices)
+    scores = tf.gather(scores_one_img, indices)
+    return tf.where(mask_stack), scores
 
 class MaskLayer(tf.keras.layers.Layer):
-    def __init__(self, n_cls=3, crop_layer=0, crop_size=32, min_score = 0.35, conv_channels=64, **kwargs):
+    def __init__(self, n_cls=3, crop_layer=0, crop_size=32, n_convs=3, conv_channels=48, min_iou=0.5, min_score=0.2, min_overlap=0.2, **kwargs):
         super(MaskLayer, self).__init__(**kwargs)
         self._config_dict = {
             'n_cls': n_cls,
             'crop_layer': crop_layer,
             'crop_size': crop_size,
+            'n_convs': n_convs,
             'min_score': min_score,
+            'min_iou': min_iou,
+            'min_overlap': min_overlap,
             'conv_channels': conv_channels,
         }
 
@@ -19,49 +77,90 @@ class MaskLayer(tf.keras.layers.Layer):
     def build(self, input_shape, **kwargs):
         n_cls = self._config_dict['n_cls']
         conv_channels = self._config_dict['conv_channels']
-        self._mask_block = [
-            BatchConv2D(conv_channels, name='mask_conv1'),
-            BatchConv2D(conv_channels, name='mask_conv1'),
-        ]
-        self._mask_out = tf.keras.layers.Conv2D(n_cls, 3, name='mask_out')
+        n_convs = self._config_dict['n_convs']
+
+        self._mask_block = []
+        for k in range(n_convs):
+            self._mask_block.append(BatchConv2D(conv_channels, name=f'mask_conv{k+1}'))
+        self._mask_out = tf.keras.layers.Conv2D(n_cls, 1, name='mask_out')
 
         super(MaskLayer,self).build(input_shape)
 
     def call(self, inputs, training=None):
-        labels, model_out = inputs
-
+        (decoder_out, bboxes, scores, cls), labels = inputs
+        _, h, w, _ = decoder_out['0'].get_shape()
         crop_size = self._config_dict['crop_size']
         crop_layer = self._config_dict['crop_layer']
 
-        bboxes = model_out['bboxes_out']
-        scores = model_out['bboxes_score_out']
-        if labels is not None:
-            cls = labels['class']
+        # bboxes = model_out['bboxes_out']
+        # scores = model_out['bboxes_score_out']
+        if training:
+            gt_bboxes = tf.cast(labels['bboxes'], tf.float32) / [h, w, h, w]
+            _, mIds, mIous, _ = ragged_box_matching(bboxes, gt_bboxes)
+            is_broken = tf.gather(labels['is_broken'], mIds, batch_dims=1)
+            mIous = tf.where(is_broken, 0.0, mIous)
+            indices = tf.where(mIous > self._config_dict['min_iou'])
+            indices = tf.RaggedTensor.from_value_rowids(indices[:,1], indices[:,0])
+            mIds = tf.gather(mIds, indices, batch_dims=1)
+            bboxes = tf.gather(bboxes, indices, batch_dims=1)
+            scores = tf.gather(scores, indices, batch_dims=1)
         else:
-            cls = tf.argmax(model_out['cls'], axis=-1, output_type=tf.int32)
-        cls_repeat = tf.repeat(cls, bboxes.row_lengths())
+            indices = tf.where(scores > self._config_dict['min_score'])
+            indices = tf.RaggedTensor.from_value_rowids(indices[:,1], indices[:,0])
+            bboxes = tf.gather(bboxes, indices, batch_dims=1)
+            scores = tf.gather(scores, indices, batch_dims=1)
 
-        features = model_out['decoder_out'][str(crop_layer)]
+        features = decoder_out[str(crop_layer)]
         feature_crops = crop_features(features, bboxes, crop_size)
 
         x = feature_crops
         for layer in self._mask_block:
             x = layer(x)
         masks = self._mask_out(x)
+
+        # if labels is not None:
+        #     cls = labels['class']
+        # else:
+        #     cls = tf.argmax(model_out['cls'], axis=-1, output_type=tf.int32)
+        cls_repeat = tf.repeat(cls, bboxes.row_lengths())
         masks = tf.gather(masks, cls_repeat[:, None], axis=-1, batch_dims=1)
-        return masks
+        masks = tf.RaggedTensor.from_row_starts(masks, bboxes.row_starts())
+
+        if training:
+            return {
+                'masks': masks,
+                'mask_bboxes': bboxes,
+                'mask_mIds': mIds,
+            }
+        else:
+            return {
+                'masks': masks,
+                'mask_bboxes': bboxes,
+                'mask_scores': scores,
+            }
 
     def _build_losses(self, labels, model_out):
-        _h,w,_ = labels['dist_map'].get_shape()
+        _, h, w, _ = labels['dist_map'].get_shape()
         crop_size = self._config_dict['crop_size']
-        bboxes = model_out['bboxes_out']
-        gt_bboxes = labels['bboxes']
-        _, matched_ids, matched_ious, _ = ragged_box_matching(bboxes, gt_bboxes)
+        bboxes = model_out['mask_bboxes']
+        matched_ids = model_out['mask_mIds']
         gt_masks = crop_masks(labels['mask_indices'], bboxes, matched_ids, crop_size, h, w)
-        pred_masks = model_out['masks']
-
-        pos_indices = tf.where(matched_ious.values > self._config_dict['min_score'])
-        gt_masks = tf.gather_nd(gt_masks.values, pos_indices)
-        pred_masks = tf.gather_nd(pred_masks.values, pos_indices)
-        mask_loss = tf.losses.binary_crossentropy(gt_masks, pred_masks, from_logits=True)
+        pred_masks = model_out['masks'].values
+        mask_loss = tf.reduce_mean(tf.losses.binary_crossentropy(gt_masks, pred_masks, from_logits=True))
         return mask_loss
+
+    def predict(self, inputs):
+        decoder_out, bboxes, scores, cls = inputs
+        _, h, w, _ = decoder_out['0'].get_shape()
+        min_overlap = self._config_dict['min_overlap']
+        layer_out = self((inputs, None), training=False)
+        scores = layer_out['mask_scores']
+        masks = tf.sigmoid(layer_out['masks'])
+        boxes = layer_out['mask_bboxes']
+
+        mask_indices, scores = tf.map_fn(
+            lambda x: decode_one_img(x[0], x[1], x[2], min_overlap, h, w),
+            [masks, boxes, scores],
+            fn_output_signature=(tf.RaggedTensorSpec((None,3), tf.int64, 0), tf.RaggedTensorSpec((None,), tf.float32, 0)),
+        )
+        return mask_indices, scores
--- cellcutter/alpha/modeling/seg_model.py
+++ cellcutter/alpha/modeling/seg_model.py
@@ -5,44 +5,25 @@ from .common import *
 from .unet import *
 
 class SegModel(tf.keras.Model):
-    def __init__(self, encoder, decoder, n_cls = 3, crop_size=64, iou_low_threshold=.3):
+    def __init__(self, encoder, decoder, mask_layer):
         super(SegModel,self).__init__()
 
         self._config_dict = {
             'encoder': encoder,
             'decoder': decoder,
-            'n_cls': n_cls,
-            'crop_size': crop_size,
-            'iou_low_threshold': iou_low_threshold,
+            'mask_layer': mask_layer,
         }
         self._encoder = encoder
         self._decoder = decoder
-
+        self._mask_layer = mask_layer
         self._build_metrics()
-
-        conv_block = [
-            BatchConv2D(64, name='out_conv1'),
-            BatchConv2D(64, name='out_conv2'),
-        ]
-        self._conv_block = conv_block
-        self._segment = layers.Conv2D(self._config_dict['n_cls'], 3, padding='same', name='segment')
-        self._mixing = layers.Conv2D(1, 1, name='mixing')
-
-        regression_block = [
-            layers.MaxPool2D((4,4), name='regression_pooling'),
-            layers.Flatten(name='regression_flatten'),
-            layers.Dense(512, activation='relu', name='regression_dense1'),
-            layers.Dense(512, activation='relu', name='regression_dense2'),
-            layers.Dense(self._config_dict['n_cls'], name='regression_out'),
-        ]
-        self._regression_block = regression_block
 
     def get_config(self):
         return self._config_dict
 
     def _build_metrics(self):
         metrics = []
-        metric_names = ['model_loss', 'mask_loss', 'regression_loss',]
+        metric_names = ['model_loss', 'mask_loss',]
         for name in metric_names:
             metrics.append(tf.keras.metrics.Mean(name, dtype=tf.float32))
         self._metrics = metrics
@@ -58,13 +39,6 @@ class SegModel(tf.keras.Model):
     @property
     def metrics(self):
         return self._metrics
-
-    # def _binary_iou(self, gt_masks, masks):
-    #     gt_mask_b = gt_masks > 0.5
-    #     mask_b = masks > 0.0
-    #     intersec = tf.math.count_nonzero(tf.logical_and(gt_mask_b, mask_b), axis=(1,2,3))
-    #     union = tf.math.count_nonzero(tf.logical_or(gt_mask_b, mask_b), axis=(1,2,3))
-    #     return intersec/union
 
     def _adjust_bbox(self, bbox):
         '''
@@ -85,83 +59,27 @@ class SegModel(tf.keras.Model):
         bbox_out = tf.RaggedTensor.from_row_starts(bbox_out, row_starts)
         return tf.cast(bbox_out, tf.float32)
 
-    def do_call(self, inputs, training = None):
-        data, labels = inputs
-        imgs = data['image']
+    def call(self, inputs, training = None):
+        (imgs, bboxes, scores, cls), labels = inputs
         features = self._encoder(imgs, training=training)
         outputs = self._decoder(features, training=training)
-
-        bboxes = data['proposal_bboxes']
-        crop_size = self._config_dict['crop_size']
-        cls = data['class']
-        cls_repeat = tf.repeat(cls, bboxes.row_lengths())
-        actual_bboxes = self._adjust_bbox(bboxes)
-        feature_crops = crop_features(outputs['0'], actual_bboxes, crop_size)
-        proposals = tf.ensure_shape(data['proposal'], (None, 544,704))
-        proposal_crops = crop_proposals(proposals, actual_bboxes, crop_size)
-
-        x = feature_crops
-        for layer in self._conv_block:
-            x = layer(x, training=training)
-        pre_masks = self._segment(x)
-        pre_masks = tf.gather(pre_masks, cls_repeat[:,None], axis=-1, batch_dims=1)
-        masks = self._mixing(tf.concat([pre_masks, proposal_crops], axis=-1), training=training)
-
-        for layer in self._regression_block:
-            x = layer(x, training=training)
-        regression_out = tf.gather(x, cls_repeat[:,None], axis=-1, batch_dims=1)
-
-        return {
-            'masks': masks,
-            'regressions': regression_out,
-            'actual_bboxes': actual_bboxes,
-        }
-
-    def _compute_losses(self, labels, model_out):
-        masks = model_out['masks']
-        _,height,width,_ = labels['weights'].get_shape()
-        crop_size = self._config_dict['crop_size']
-        gt_masks = crop_masks(
-            labels['mask_indices'],
-            model_out['actual_bboxes'],
-            labels['matched_indices'],
-            crop_size, height, width,
-        )
-
-        regression_out = model_out['regressions'],
-        proposal_ious = labels['matched_ious'].merge_dims(0,1)
-        regression_loss = tf.reduce_mean(
-            tf.keras.losses.huber(proposal_ious, regression_out, 0.5)
-        )
-
-        gt_masks = tf.cast(gt_masks>.5, tf.float32)
-        #mask_loss = tf.reduce_mean(tf.keras.losses.binary_crossentropy(gt_masks, masks, from_logits=True), axis=(1,2))
-        mask_loss = tf.reduce_mean(tfa.losses.sigmoid_focal_crossentropy(gt_masks, masks, from_logits=True), axis=(1,2))
-        pos_indices = tf.where(proposal_ious >= self._config_dict['iou_low_threshold'])
-        mask_loss = tf.reduce_mean(tf.gather(mask_loss, pos_indices))
-
-        model_loss = mask_loss + regression_loss
-        return {
-            'model_loss': model_loss,
-            'mask_loss': mask_loss,
-            'regression_loss': regression_loss,
-        }
+        data = outputs, bboxes, scores, cls
+        return self._mask_layer((data, labels), training=training)
 
     def train_step(self, inputs):
         data, labels = inputs
         with tf.GradientTape() as tape:
-            model_out = self.do_call(inputs, training=True)
-            losses = self._compute_losses(labels, model_out)
-        grads = tape.gradient(losses['model_loss'], self.trainable_variables)
+            model_out = self(inputs, training=True)
+            mask_loss = self._mask_layer._compute_losses(labels, model_out)
+        grads = tape.gradient(mask_loss, self.trainable_variables)
         self.optimizer.apply_gradients((g, v) for (g, v) in zip(grads, self.trainable_variables) if g is not None)
 
-        logs = self._update_metrics(losses)
+        logs = self._update_metrics({'mask_loss': mask_loss})
         return logs
 
     def test_step(self, inputs):
         data, labels = inputs
-        model_out = self.do_call(inputs, training=False)
-        losses = self._compute_losses(labels, model_out)
-        logs = self._update_metrics(losses)
-
+        model_out = self(inputs, training=False)
+        mask_loss = self._mask_layer._compute_losses(labels, model_out)
+        logs = self._update_metrics({'mask_loss': mask_loss})
         return logs
